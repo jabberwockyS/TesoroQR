c023b771fd784c632748c6580b19b34fac6b99bc
jabberwockyS <jarasergio@outlook.com>
2014-07-09 15:41:09 -0300

    esto es previo a la publicacion. god bless you

--- TesoroQR/Controllers/InformesController.cs
+++ TesoroQR/Controllers/InformesController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TesoroQR.Models.Tesoro;
 
 namespace TesoroQR.Controllers
 {
+    [Authorize(Roles="Admin")]
     public class InformesController : Controller
     {
+        TesoroRepository repo = new TesoroRepository();
         //
         // GET: /Informes/
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ListarPartidas()
+        {
+            return View(repo.Partidas());
+        }
+
+        public ActionResult ListarJugadores(int partidaID)
+        {
+
+            return View(repo.UsuariosPorPartida(partidaID));
         }
 	}
 }
---
+++ TesoroQR/Models/Tesoro/TesoroRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TesoroQR.Models.Tesoro;
+
+namespace TesoroQR.Models.Tesoro
+{
+    public class TesoroRepository
+    {
+        JuegoDBContext db = new JuegoDBContext();
+
+
+
+        public List<Partida> Partidas()
+        {
+            return db.Partidas.ToList();
+        }
+
+        public List<Usuario> UsuariosPorPartida(int partidaID)
+        {
+            List<Usuario> usuarios = db.Usuarios.ToList();
+
+           
+
+            foreach(Usuario usuario in usuarios)
+            {
+                List<Juego> juegos =db.Juegos.Where(x => x.Jugador.UsuarioID == usuario.UsuarioID).ToList();
+
+                usuario.Juegos = juegos;
+                
+            }
+
+
+
+
+
+            
+
+            return usuarios;
+        }
+
+
+    }
+}
